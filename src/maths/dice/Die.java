b456bc17bc55b06b8940b3bb6db702772454f8cf
calvi <calvi@DESKTOP-4DC51T7.attlocal.net>
2021-02-19 01:03:09 -0500

    Implemented DieTester (100% unit test pass)

--- src/maths/dice/Die.java
+++ src/maths/dice/Die.java
@@ -5,21 +5,26 @@ import java.util.Random;
 public class Die {
 	protected int size;
 	protected int roll;
-	
+
 	private static final Random r = new Random();
-	
+
 	public Die(int size) {
 		this.size = size;
+		roll = 0;
 	}
-	
+
 	public void roll() {
-		roll = Die.r.nextInt(size) + 1; 
+		roll = Die.r.nextInt(size) + 1;
 	}
-	
+
+	public int getSize() {
+		return size;
+	}
+
 	public int getRoll() {
 		return roll;
 	}
-	
+
 	@Override
 	public Die clone() {
 		Die d = new Die(size);
