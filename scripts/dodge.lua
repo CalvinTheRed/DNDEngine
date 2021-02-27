5b9cdb9048e77acf47432e14f86153f092739c06
calvi <calvi@DESKTOP-4DC51T7.attlocal.net>
2021-02-27 17:55:37 -0500

    Dodge Event working via Lua

--- scripts/dodge.lua
+++ scripts/dodge.lua
@@ -1,5 +1,14 @@
 function invokeEvent()
 
+-- see more: http://www.luaj.org/luaj/3.0/README.html#luajava
+
 print("[LUA] Applying Dodge effect to " .. source:toString())
+
+-- find way to utilize VirtualBoard search functions
+
+effect = luajava.newInstance("dnd.effects.Dodge", source, source)
+source:addEffect(effect)
+
+print("[LUA] Done")
 
 end
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import dnd.effects.Effect;
 import gameobjects.entities.Entity;
 import gameobjects.entities.Zombie;
 import maths.Vector;
@@ -9,19 +10,17 @@ public class Main {
 	// public static Window window;
 
 	public static void main(String[] args) {
-		// initialize();
 		test();
-	}
-
-	private static void initialize() {
-		// Window.setDefaultLookAndFeelDecorated(false);
-		// window = new Window();
 	}
 
 	private static void test() {
 		Entity zombie1 = new Zombie(new Vector(30, 0, 0), new Vector(1, 0, 0));
 		zombie1.invokeTask(0);
-		zombie1.invokeQueuedEvent(0, 0, new Vector(0, 0, 0));
+		zombie1.invokeQueuedEvent(0, 0, zombie1.getPos());
+
+		for (Effect e : zombie1.getEffects()) {
+			System.out.println(e);
+		}
 
 //		zombie1.addEffect(new dnd.effects.Dodge(zombie1, zombie1));
 
